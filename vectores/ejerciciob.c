a189ff5aef7adb7c188c2c6d6046ee1b39d3c992
Jeiij <128878273+Jeiij@users.noreply.github.com>
2023-05-12 13:34:46 -0400

    Add files via upload

---
+++ vectores/ejerciciob.c
@@ -0,0 +1,23 @@
+//Paso de funciones como parametros
+#include <stdio.h>
+void aplicarFuncion(int (*funcion)(int), int num);
+int cuadrado(int x);
+int cubo(int x);
+int main() {
+ int num = 5;
+ printf("Aplicando la función cuadrado a %d: ", num);
+ aplicarFuncion(cuadrado, num);
+ printf("Aplicando la función cubo a %d: ", num);
+ aplicarFuncion(cubo, num);
+ return 0;
+}
+void aplicarFuncion(int (*funcion)(int), int num) {
+ int resultado = funcion(num);
+ printf("%d\n", resultado);
+}
+int cuadrado(int x) {
+ return x * x;
+}
+int cubo(int x) {
+ return x * x * x;
+}
